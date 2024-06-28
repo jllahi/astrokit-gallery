97ff33ffc7df37a9f080dd78c989a16f534c6ab8
jllahi <1439075+jllahi@users.noreply.github.com>
2024-06-28 19:54:58 +0200

    Disable Masonry

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,2 @@
-/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-declare module 'astro-icon/components'
-declare module 'photoswipe-dynamic-caption-plugin'
+/// <reference path="../.astro/types.d.ts" />
